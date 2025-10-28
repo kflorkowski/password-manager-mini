828883f29a68821fafed231a4ea27d1b65642178
kflorkowski <k.florkowski98@gmail.com>
2025-10-28 15:11:00 +0100

    refactor: move .img to images catalog

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
-my_pass_image = PhotoImage(file="logo.png")
+my_pass_image = PhotoImage(file="images/logo.png")
 canvas.create_image(100, 100, image=my_pass_image)
 canvas.grid(row=0, column=1)
 
